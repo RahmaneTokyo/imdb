306588cb19d963091bb2ebb55103106b1a941078
RahmaneTokyo <rahmane961@gmail.com>
2024-05-30 13:54:53 +0000

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+export default function About() {
+  return (
+      <div className='my-10 max-w-7xl mx-auto p-3 gap-4 space-y-4'>
+          <h1 className='text-2xl font-medium text-amber-600'>
+            About
+          </h1>
+          <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid doloribus dolorum esse est ipsa nulla
+              omnis repellendus temporibus ut? Eveniet facilis hic itaque nam officia suscipit voluptates? Ab, voluptatum!
+          </p>
+          <p>
+              A at consectetur culpa distinctio ducimus eligendi ratione sed ut vero vitae! Doloribus maiores molestiae
+              necessitatibus optio recusandae repellendus rerum ullam? Adipisci consectetur minima quasi qui quis.
+              Ducimus, natus, vitae!
+          </p>
+          <p>
+              Alias aliquid animi at atque aut blanditiis deserunt est exercitationem, illo illum, in iste laborum minus
+              neque placeat praesentium quam, quis quisquam quo reprehenderit saepe suscipit tempora vero voluptas
+              voluptates?
+          </p>
+      </div>
+  )
+}
