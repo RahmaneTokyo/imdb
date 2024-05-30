f3672bd2cfa7f1d7a5ee091e404efa0e7a07907b
RahmaneTokyo <rahmane961@gmail.com>
2024-05-30 13:38:46 +0000

    adding dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,22 @@ import MenuItems from "@/components/MenuItems";
 import {AiFillHome} from "react-icons/ai";
 import {BsFillInfoCircleFill} from "react-icons/bs";
 import Link from "next/link";
+import DarkModeSwitch from "@/components/DarkModeSwitch";
 
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-7xl mx-auto'>
-      <div className='flex gap-4'>
-          <MenuItems title='home' address='/' Icon={AiFillHome}/>
-          <MenuItems title='about' address='/about' Icon={BsFillInfoCircleFill}/>
-      </div>
-      <Link href="/" className='flex gap-1 items-center'>
-          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
-          <span className='text-xl hidden sm:inline'>Clone</span>
-      </Link>
+        <div className='flex gap-4'>
+            <MenuItems title='home' address='/' Icon={AiFillHome}/>
+            <MenuItems title='about' address='/about' Icon={BsFillInfoCircleFill}/>
+        </div>
+        <div className='flex items-center gap-4'>
+            <DarkModeSwitch />
+            <Link href="/" className='flex gap-1 items-center'>
+                <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
+                <span className='text-xl hidden sm:inline'>Clone</span>
+            </Link>
+        </div>
     </div>
   )
 }
